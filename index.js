4a39716905cb7a7fe305cba67db0b8ebe0c9dc8c
mahfuz-antor <antorcnj2014@gmail.com>
2022-12-25 14:38:15 +0600

    services collection is connected

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// mongoDB uri and client here
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.onckrto.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctor_portal").collection("services");
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Doctor portal app!");
 });
 
 app.listen(port, () => {
-  console.log(`doctor portal Doctor ${port}`);
+  console.log(`Doctor Portal App Port: ${port}`);
 });
